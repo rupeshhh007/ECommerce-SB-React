134907bf722e5cca952cb60731c5bd64b79e0647
Rupesh Tripathi <rupeshstripathi1234@gmail.com>
2025-05-23 10:49:08 +0530

    building up backend structure

--- e-commerce-sb/src/main/java/com/rupesh/e_commerce/Repository/ProductRepository.java
+++ e-commerce-sb/src/main/java/com/rupesh/e_commerce/Repository/ProductRepository.java
@@ -2,6 +2,8 @@ package com.rupesh.e_commerce.Repository;
 
 import com.rupesh.e_commerce.Models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product,Integer> {
 }
--- e-commerce-sb/src/main/java/com/rupesh/e_commerce/Services/ProductService.java
+++ e-commerce-sb/src/main/java/com/rupesh/e_commerce/Services/ProductService.java
@@ -1,12 +1,18 @@
 package com.rupesh.e_commerce.Services;
 
 import com.rupesh.e_commerce.Models.Product;
+import com.rupesh.e_commerce.Repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class ProductService {
+    @Autowired
+    private ProductRepository repo;
+
     public List<Product> getAllProducts() {
+        return repo.findAll();
     }
 }
